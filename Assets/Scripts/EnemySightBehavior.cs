49dacd8256cb805a9c08288976e602e4c063544b
Arnaud Frézet <arnaudfrezet@gmail.com>
2016-06-25 16:14:22 +0200

    Add enemy sight behavior

---
+++ Assets/Scripts/EnemySightBehavior.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySightBehavior : MonoBehaviour {
+
+	public float fieldOfViewAngle = 110f;
+
+	public float visionDistance = 10;
+
+	protected Vector3 lastViewedPosition;
+
+	// Use this for initialization
+	void Awake()
+	{
+		visionDistance = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (IsPlayerVisible ()) {
+			Debug.Log ("VISIBLE 0==0");
+		}
+
+		//Debug.Log ("-----------------------------------");
+	}
+
+
+	bool IsPlayerVisible()
+	{
+		bool isVisible = false;
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		Vector3 playerPosition = player.transform.position;
+		Vector3 skeletonPosition = gameObject.transform.position;
+
+		Vector3 direction = playerPosition - skeletonPosition;
+		float angle = Vector3.Angle(direction, transform.forward);	
+		float distance = Vector3.Distance (playerPosition, skeletonPosition);
+
+		float test = player.transform.localEulerAngles.y - gameObject.transform.localEulerAngles.y; 
+
+		// if the player is in view angle and in distance vision range
+		if(distance <= visionDistance && angle < fieldOfViewAngle * 0.5f) {
+			isVisible = true;
+		}
+
+		//Debug.Log ("rotation");
+		//Debug.Log (test);
+
+
+		return isVisible;
+	}
+}
---
+++ Assets/Scripts/LightDiminuer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LightDiminuer : MonoBehaviour {
+
+	public float intensityRate = 0.001F;
+
+	public float ambientIntensity = 3;
+
+	private Light[] lights;
+
+	private int frameCount = 0;
+
+	private Light playerLight;
+
+	private Light ambientPlayerLight;
+
+	// Use this for initialization
+	void Awake () {
+		lights = FindObjectsOfType(typeof(Light)) as Light[];
+
+		playerLight = GameObject.FindGameObjectWithTag ("PlayerLight").GetComponent<Light>();
+		ambientPlayerLight = GameObject.FindGameObjectWithTag ("AmbientPlayerLight").GetComponent<Light>();
+
+		playerLight.intensity = 5;
+		playerLight.range = 15;
+
+		ambientPlayerLight.intensity = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerLight.intensity == 0) {
+			ambientPlayerLight.intensity = ambientIntensity;
+		} else {
+			playerLight.intensity = playerLight.intensity - intensityRate;
+			//playerLight.transform.position = new Vector3(Mathf.PerlinNoise(0, 1) * 5, Mathf.PerlinNoise(0, 1), Mathf.PerlinNoise(0, 1) * 5);
+		}
+	}
+}
