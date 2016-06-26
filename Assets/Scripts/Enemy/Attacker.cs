074f384c608ec89936b14ae223dc3ae9517c929b
lutangar <johan.dufour@gmail.com>
2016-06-26 12:18:51 +0200

    add game over

--- Assets/Scripts/Enemy/Attacker.cs
+++ Assets/Scripts/Enemy/Attacker.cs
@@ -5,20 +5,22 @@ public class Attacker : MonoBehaviour {
 
 	public float attackDistance = 1;
 	public bool attacking;
-	public float attackDuration = .9f;
+	public float attackDuration = .7f;
 
 	private GameObject player;
+	private PlayerHealth playerHealth;
 	private EnemySightBehavior enemySight;
-	public float attackStartedTime;
+	private float attackStartedTime;
 
 	// Use this for initialization
 	void Awake () {
 		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent<PlayerHealth>();
 		enemySight = GetComponent<EnemySightBehavior> ();
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
 		if (enemySight.IsPlayerVisible () && isPlayerAttackable ()) {
 			if (attacking == false) {
@@ -28,15 +30,25 @@ public class Attacker : MonoBehaviour {
 		}
 
 		if ((Time.time - attackStartedTime) > attackDuration) {
+			if (attacking == true) {
+				hit (playerHealth);
+			}
 			attacking = false;
 		}
+	}
+		
+	public void hit(PlayerHealth playerHealth)
+	{
+		var damage = Random.Range(10, 20);
+
+		playerHealth.TakeDamage(damage);
 	}
 
 	public bool isPlayerAttackable()
 	{
 		float distance = Vector3.Distance (transform.position, player.transform.position);
 
-		return attackDistance >= distance;
+		return (attackDistance >= distance && !playerHealth.isDead());
 	}
 
 	public bool isAttacking()
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,39 +1,65 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
-	public float health = 100f;                         // How much health the player has left.
+	private float health = 100f;                         // How much health the player has left.
 	public float resetAfterDeathTime = 5f;              // How much time from the player dying to the level reseting.
 	public AudioClip deathClip;                         // The sound effect of the player dying.
-
+	public Text lifeBar;
+	private GameObject gameOver;
+	private bool dead = false;
 
 	private Animator anim;                              // Reference to the animator component.
 	private LastPlayerSighting lastPlayerSighting;      // Reference to the LastPlayerSighting script.
 	private float timer;                                // A timer for counting to the reset of the level once the player is dead.
 	private bool playerDead;                            // A bool to show if the player is dead or not.
 
-
 	void Awake ()
 	{
 		// Setting up the references.
-		anim = GetComponent<Animator>();
+		anim = transform.parent.gameObject.GetComponent<Animator>();
 		lastPlayerSighting = GameObject.FindGameObjectWithTag("Player").GetComponent<LastPlayerSighting>();
+		gameOver = GameObject.Find ("GameOver");
 	}
-
-
+		
 	void Update ()
 	{
-		// If health is less than or equal to 0...
-		if(health <= 0f)
-		{
-			// @todo do stuff
+		if (!isDead ()) {
+			lifeBar.text = health.ToString ("0");
 		}
 	}
 
 	public void TakeDamage (float amount)
 	{
+		if (health <= 0f) {
+			health = 0;
+			if (!isDead ()) {
+				die ();
+			}
+
+			return;
+		}
+
 		// Decrement the player's health by amount.
 		health -= amount;
+		if (health <= 0f) {
+			health = 0;
+		}
 	}
+		
+	public void die()
+	{
+		dead = true;
+		health = 0;
+		gameOver.SetActive (true);
+		anim.SetTrigger ("Dead");
+	}
+
+	public bool isDead()
+	{
+		return dead;
+	}
+
 }
